73fb983e31ac10737195a8452d35214b7a40d2e0
JakeThe28th <42324960+JakeThe28th@users.noreply.github.com>
2021-05-10 17:55:53 -0400

    Initial commit

---
+++ src/main/main.java
@@ -0,0 +1,26 @@
+package main;
+
+public class main {
+	//My first serious java project, better put some to-do's here.
+	//In order of importance
+	//[?] Learn java
+	//[ ] write a basic .obj file
+	//[ ] read minecraft json files
+	//[ ] convert minecraft json files to obj files
+	//[ ] create nbt modules
+	//[ ] read minecraft level data
+	//[ ] export minecraft level data into .obj files
+	//[ ] configuration
+	
+	//STRETCH GOALS
+	//[ ] Texture sheet packing
+	//[ ] Biome colors
+	//[ ] Resource packs as seperate rather than conbined
+	//[ ] a GUI.
+	//[ ] minecraft style smooth and flat lighting (flat will be easy, smooth not so much...?)
+	
+	//OPTIONAL
+	//[ ] Read schematic files and structure block NBT data
+	
+	//5/10/2021 (c) Jake 28
+}
